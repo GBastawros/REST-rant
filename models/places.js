b756fbd025868fbfe719836efa07bd591a94720c
George Bastawros <george.bastawros@outlook.com>
2022-10-02 10:26:42 -0500

    New places ca be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/lily-banse--YHSwy6uqvk-unsplash.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/chad-montano-MqT0asuoIcU-unsplash.jpg'
+}]
