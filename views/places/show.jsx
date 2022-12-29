97f9e90222957972de8a8f54d88e37d59f80b0e6
George Bastawros <106220865+GBastawros@users.noreply.github.com>
2022-12-28 22:08:15 -0600

    Update show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,120 +1,112 @@
 const React = require('react')
 const Def = require('../default')
 
-function show(data) {
-    let comments = (
-        <h3 className="inactive">
-            No comment yet!
-        </h3>
-    )
-    let rating = (
-        <h3 className="inactive">
-            not yet rated
-        </h3>
-    )
-    if (data.place.comments.length) {
-        let sunRating = data.place.comments.reduce((tot, c)=>{
-            return tot + c.stars
-        }, 0)
-        let averageRating = Math.round(sunRating / data.place.comments.length)
-        let stars = ""
-        for (let i = 0; i<averageRating; i++){
-            stars += "â­ï¸"
-        }
-        rating = (
-            <h3>
-                {stars} stars
-            </h3>
-        )
-        comments = data.place.comments.map(c => {
-            return (
-                <div className="border col-sm-4">
-                    <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-                    <h4>{c.content}</h4>
-                    <h3>
-                        <stong>- {c.author}</stong>
-                    </h3>
-                    <h4>Rating: {c.stars}</h4>
-                </div>
-            )
-        })
+function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No Comments Yet!
+    </h3>
+  )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    },0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i=0; i < averageRating; i++) {
+      stars += ' ⭐ '
     }
-
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+          <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+            <form method='POST' action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+              <input type="submit" className='btn btn-danger' value='Delete Comment'/>
+            </form>
+        </div>
+      )
+    })
+  }
     return (
-        <Def>
-            <main>
-                <div className="row">
-                    <div className="col-sm-6">
-                        <img src={data.place.pic} alt={data.place.name} />
-                        <h3>
-                            located in {data.place.city}, {data.place.state}
-                        </h3>
-                    </div>
-                </div>
-                <h1>{data.place.name}</h1>
-                <h2>rating</h2>
-                 {rating}
-                <h2>Description</h2>
+    <Def>
+       <main>
+       <div className="row">
+              <div className="col-sm-6">
+                <img className='home-img' src={data.place.pic} alt={data.place.name} />
                 <h3>
-                    {data.place.showEstablished()}
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className="col-sm-6">
+                <h1>{ data.place.name }</h1>
+                <h2>
+                  Rating
+                </h2>
+                <h5>{rating}</h5>
+                <br />
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  {data.place.showEstablished()}
                 </h3>
                 <h4>
-                    serving {data.place.cuisine}
+                  Serving {data.place.cuisines}
                 </h4>
-                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
-                <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}>
+                <br />
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">Edit <i className='fas fa-pen'></i></a>  
+                <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
-                        Delete
+                        Delete <i className='fas fa-trash-alt'></i>
                     </button>
-                    <hr />
-                    <h2>Comments</h2>
-                    {comments}
                 </form>
-                <form method="POST" action={`/places/${data.place.id}`} id="comment">
-                    <div>
-                        <div>
-                            <input
-                                id="rant"
-                                name="rant"
-                                type="checkbox"
-                                value="Rant"
-                            />
-                            <label htmlFor="rant">Rant</label>
-                        </div>
-                        <div>
-                            <label htmlFor="author">Author</label>
-                            <input
-                                id="author"
-                                name="author"
-                                type="text"
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="Content">Content</label>
-                            <input
-                                id="content"
-                                name="content"
-                                type="textarea"
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="stars">Star Rating</label>
-                            <input
-                                id="constarstent"
-                                name="stars"
-                                type="range"
-                                min="1"
-                                max="5"
-                                step="0.5"
-                            />
-                        </div>
-                    </div>
-                    <input className="btn btn-primary" type="submit" value="Add Comment" />
-                </form>
-            </main>
-        </Def>
+              </div>
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            <h5>{comments}</h5>
+            <hr />
+            <h2>Want to add your own comment?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4 range">
+                  <label className='form-range-label' htmlFor="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-range" />
+                </div>
+                <div className="form-group col-sm-2 check">
+                  <label className='form-check-label' htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-check" />
+                </div>
+                </div>
+                <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
+        </main> 
+    </Def>
     )
 }
 
